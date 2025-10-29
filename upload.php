90f4e0904ace826913371fc40a6fcf357052c2a1
nneko_gtn <tatyananotabene@gmail.com>
2025-10-29 21:03:03 +0300

    Add files via upload

--- upload.php
+++ upload.php
@@ -1,15 +1,27 @@
 <?php
-if (empty($_FILES['file_name']['name'])) {
+if (empty($_POST['file_name'])) {
     header('Location: index.html');
     exit;
 }
+
+if (empty($_FILES['content']['name']) || $_FILES['content']['error'] !== UPLOAD_ERR_OK) {
+    header('Location: index.html');
+    exit;
+}
+
 $uploaddir = './upload';
-$uploadfile = $uploaddir . '/' . basename($_FILES['file_name']['name']);
 
-if (move_uploaded_file($_FILES['file_name']['tmp_name'], $uploadfile)) {
-        echo 'Файл загружен' . PHP_EOL . 'Путь к файлу:' . $uploadfile . PHP_EOL . 'Размер файла: ' . $_FILES['file_name']['size'];
+if (!file_exists($uploaddir)) {
+    mkdir($uploaddir, 0755, true);
+}
+
+$uploadfile = $uploaddir . '/' . basename($_POST['file_name']);
+
+if (move_uploaded_file($_FILES['content']['tmp_name'], $uploadfile)) {
+    echo 'Файл успешно загружен!' . '<br>';
+    echo 'Путь к файлу: ' . realpath($uploadfile) . '<br>';
+    echo 'Размер файла: ' . $_FILES['content']['size'] . ' байт';
 } else {
-        header('Location: index.html');
-        exit;
-        }
-
+    header('Location: index.html');
+    exit;
+}
